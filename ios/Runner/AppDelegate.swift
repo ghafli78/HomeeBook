8e9ae3b45abef28f4e7c960edbb8a8908c2b3326
ghafli78 <ghafli78@gmail.com>
2025-09-20 11:04:37 +0400

    Update app version, add GoogleService-Info.plist, update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,14 @@
-import Flutter
 import UIKit
+import Flutter
+import FirebaseCore   // أضفنا FirebaseCore هنا
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure()   // هذا السطر يربط التطبيق بـ GoogleService-Info.plist
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
